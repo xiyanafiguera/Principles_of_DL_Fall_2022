8f055c0ba0289e093a4a9c71eb4e163bf3e7779d
Xiyana Figuera <112554275+xiyanafiguera@users.noreply.github.com>
2024-09-29 14:40:34 +0900

    Add assignment 5 files

---
+++ 5- bert_fine_assignmnet-xiyanafiguera-main/pytest.py
@@ -0,0 +1,29 @@
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+import typing
+
+def is_float(element: typing.Any) -> bool:
+    try:
+        float(element)
+        return True
+    except ValueError:
+        return False
+
+def execute_notebook(notebook_filename):
+	with open(notebook_filename) as ff:
+		nb_in = nbformat.read(ff, nbformat.NO_CONVERT)
+		
+	ep = ExecutePreprocessor(timeout=500, kernel_name='python')
+	nb_out = ep.preprocess(nb_in)
+	return nb_out
+
+
+def test_bert():
+	nb_out = execute_notebook('BERT_fine_assignment.ipynb')
+	# load the last cell's output
+	print(float(nb_out[0]['cells'][-1]['outputs'][-1]['text']))
+	assert is_float(nb_out[0]['cells'][-1]['outputs'][-1]['text']) == True
+	assert float(nb_out[0]['cells'][-1]['outputs'][-1]['text']) >0.5
+
+if __name__ == '__main__':
+	test_bert()
